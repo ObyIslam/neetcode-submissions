247f144cf348ecee87d61ec3b785f18eb0295f9f
Oby Islam <123547115+ObyIslam@users.noreply.github.com>
2026-07-13 11:47:47 +0100

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int[] DailyTemperatures(int[] temperatures) {
+        
+        Stack<int> stack = new();
+
+        int[] result = new int[temperatures.Length];
+
+        for(int i = 0; i < temperatures.Length; i++){
+
+            while(stack.Count > 0 && temperatures[i] > temperatures[stack.Peek()]){
+                int previousDay = stack.Pop();
+
+                result[previousDay] = i - previousDay;
+            }
+
+            stack.Push(i);
+        }
+
+        return result;
+    }
+}
