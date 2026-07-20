a335e54f581a717cc77586152318b796f3be3aaf
Oby Islam <123547115+ObyIslam@users.noreply.github.com>
2026-07-20 23:41:11 +0100

    Add: hand-of-straights - submission-0

---
+++ Data Structures & Algorithms/hand-of-straights/submission-0.cs
@@ -0,0 +1,49 @@
+public class Solution {
+    public bool IsNStraightHand(int[] hand, int groupSize) {
+        
+
+
+        if(hand.Length % groupSize != 0){
+            return false;
+        }
+
+        Dictionary<int, int> count = new();
+        foreach(int card in hand){
+            if(!count.ContainsKey(card)){
+                count[card] = 0;
+            }
+
+            count[card]++;
+        }
+
+        PriorityQueue<int, int> minHeap = new();
+        foreach(int card in count.Keys){
+            minHeap.Enqueue(card,card);
+        }
+
+        while(minHeap.Count > 0){
+            
+            int first = minHeap.Peek();
+
+            for(int i = 0; i < groupSize; i++){
+                int card = first + i;
+
+                if(!count.ContainsKey(card)){
+                    return false;
+                }
+
+                count[card]--;
+
+                if(count[card] == 0){
+                    if(card != minHeap.Peek()){
+                        return false;
+                    }
+                    minHeap.Dequeue();
+                }
+            }
+
+
+        }
+        return true;
+    }
+}
