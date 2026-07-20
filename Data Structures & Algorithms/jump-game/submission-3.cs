d93eae243e5fbb1e979221e989c2d800d995c940
Oby Islam <123547115+ObyIslam@users.noreply.github.com>
2026-07-20 22:56:04 +0100

    Add: jump-game - submission-3

---
+++ Data Structures & Algorithms/jump-game/submission-3.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public bool CanJump(int[] nums) {
+        
+        int maxReach = 0;
+
+        for(int i = 0; i < nums.Length; i++){
+
+            if (i > maxReach){
+                return false;
+            }
+
+            maxReach = Math.Max(maxReach, nums[i] + i);
+        }
+
+        return true;
+    }
+}
