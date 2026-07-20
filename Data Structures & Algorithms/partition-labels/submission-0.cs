8647947494b076fb7d36a0982dc78bc8c97b1749
Oby Islam <123547115+ObyIslam@users.noreply.github.com>
2026-07-20 23:56:22 +0100

    Add: partition-labels - submission-0

---
+++ Data Structures & Algorithms/partition-labels/submission-0.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public List<int> PartitionLabels(string s) {
+        
+        Dictionary<int,int> last = new();
+
+        for(int i = 0; i < s.Length; i++){
+            last[s[i]] = i;
+        }
+
+        List<int> result = new();
+
+        int start = 0;
+        int end = 0;
+
+        for(int i = 0; i < s.Length; i++){
+
+            end = Math.Max(end , last[s[i]]);
+
+            if(i == end){
+                result.Add(i - start +1);
+                start = i + 1;
+            }
+        }
+
+        return result;
+
+    }
+}
